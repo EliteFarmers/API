c1ddcf798547f895a37eac51045b2979052ebb19
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-07-07 20:42:37 -0400

    Configure leaderboards

--- EliteAPI/Config/Settings/LeaderboardSettings.cs
+++ EliteAPI/Config/Settings/LeaderboardSettings.cs
@@ -4,7 +4,10 @@ public class ConfigLeaderboardSettings {
     public int CompleteRefreshInterval { get; set; }
     // ReSharper disable once CollectionNeverUpdated.Global
     public List<Leaderboard> Leaderboards { get; set; } = new();
+    public Dictionary<string, CollectionLeaderboard> CollectionLeaderboards { get; set; } = new();
+    public Dictionary<string, SkillLeaderboard> SkillLeaderboards { get; set; } = new();
 }
+
 // ReSharper disable once ClassNeverInstantiated.Global
 public class Leaderboard
 {
@@ -15,3 +18,14 @@ public class Leaderboard
     public int ScoreFormat { get; set; } = 1;
 }
 
+// ReSharper disable once ClassNeverInstantiated.Global
+public class CollectionLeaderboard {
+    public required string Id { get; set; }
+    public required string Title { get; set; }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class SkillLeaderboard {
+    public required string Id { get; set; }
+    public required string Title { get; set; }
+}
