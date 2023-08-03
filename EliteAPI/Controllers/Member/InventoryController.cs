8c4ac73c0928d7a99f24ca29d6d8c87cc8a39646
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-08-02 23:57:19 -0400

    Start working on inventory parsing

---
+++ EliteAPI/Controllers/Member/InventoryController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using EliteAPI.Models.DTOs.Outgoing;
+using EliteAPI.Parsers.Inventories;
+using EliteAPI.Services.MemberService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EliteAPI.Controllers.Member; 
+
+[Route("[controller]")]
+[ApiController]
+public class InventoryController : Controller {
+    
+    private readonly IMemberService _memberService;
+    private readonly IMapper _mapper;
+
+    public InventoryController(IMemberService memberService, IMapper mapper) {
+        _memberService = memberService;
+        _mapper = mapper;
+    }
+    
+    // GET <InventoryController>/7da0c47581dc42b4962118f8049147b7/
+    [HttpGet("{playerUuid}")]
+    public async Task<ActionResult<DecodedInventoriesDto>> GetSelectedInventory(string playerUuid) {
+        var uuid = playerUuid.Replace("-", "");
+        await _memberService.UpdatePlayerIfNeeded(uuid);
+
+        var query = await _memberService.ProfileMemberQuery(uuid);
+        if (query is null) return NotFound("Inventory not found.");
+        
+        var inventory = await query
+            .Where(p => p.IsSelected)
+            .Include(p => p.Inventories)
+            .Select(p => p.Inventories)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+        
+        if (inventory is null) return NotFound("Inventory not found.");
+        
+        var decoded = await inventory.DecodeToNbt();
+        
+        return Ok(decoded);
+    }
+}
--- EliteAPI/Parsers/Inventories/InventoryParser.cs
+++ EliteAPI/Parsers/Inventories/InventoryParser.cs
@@ -1,7 +1,10 @@
-﻿using EliteAPI.Models.DTOs.Incoming;
+﻿using System.IO.Compression;
+using System.Text.Json;
+using EliteAPI.Models.DTOs.Incoming;
+using EliteAPI.Models.DTOs.Outgoing;
 using EliteAPI.Models.Entities.Hypixel;
-using fNbt;
 using Microsoft.IdentityModel.Tokens;
+using SharpNBT;
 
 namespace EliteAPI.Parsers.Inventories; 
 
@@ -12,6 +15,8 @@ public static class InventoryParser {
         member.Api.Vault = memberData.PersonalVaultContents is not null;
         
         member.Inventories.ProfileMemberId = member.Id;
+        member.Inventories.ProfileMember = member;
+        
         member.Inventories.LastUpdated = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 
         if (memberData.InventoryContents is null) return;
@@ -29,19 +34,34 @@ public static class InventoryParser {
 
         member.Inventories.Backpacks = memberData.BackpackContents?.Values.Select(x => x.Data).ToList();
     }
-    
-    private static async Task<NbtTag?> DecodeToNbt(this RawInventoryData inventory) {
-        if (inventory.Data.IsNullOrEmpty()) return null;
+
+    public static async Task<DecodedInventoriesDto> DecodeToNbt(this Models.Entities.Hypixel.Inventories inventories) {
+        return new DecodedInventoriesDto {
+            Armor = await DecodeToNbt(inventories.Armor),
+            EnderChest = await DecodeToNbt(inventories.EnderChest),
+            Equipment = await DecodeToNbt(inventories.Equipment),
+            Inventory = await DecodeToNbt(inventories.Inventory),
+            PersonalVault = await DecodeToNbt(inventories.PersonalVault),
+            TalismanBag = await DecodeToNbt(inventories.TalismanBag),
+            Wardrobe = await DecodeToNbt(inventories.Wardrobe),
+            Backpacks = inventories.Backpacks?.Select(async b => await DecodeToNbt(b)).ToList<object>()
+        };
+    }
+
+    private static async Task<object?> DecodeToNbt(string? data) {
+        if (data is null || data.IsNullOrEmpty()) return null;
         
         try {
-            var decodedInventory = Convert.FromBase64String(inventory.Data);
+            var decodedInventory = Convert.FromBase64String(data);
 
             await using var compressedStream = new MemoryStream(decodedInventory);
 
-            var file = new NbtFile();
-            file.LoadFromStream(compressedStream, NbtCompression.GZip);
-            
-            return file.RootTag;
+            await using var reader = new TagReader(new GZipStream(compressedStream, CompressionMode.Decompress),
+                FormatOptions.Java);
+
+            var tag = reader.ReadTag<CompoundTag>();
+            var stringTag = tag.ToJsonString();
+            return JsonSerializer.Deserialize<object>(stringTag);
         } catch (Exception e) {
             Console.WriteLine(e);
             return null;
