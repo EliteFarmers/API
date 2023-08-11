b20f816d9a04693b4931c2b42b8cc9b351e9d5d9
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-08-11 17:56:57 -0400

    Improve collection grouping

--- EliteAPI/Services/TimescaleService/TimescaleService.cs
+++ EliteAPI/Services/TimescaleService/TimescaleService.cs
@@ -1,6 +1,7 @@
 ﻿using EliteAPI.Data;
 using EliteAPI.Models.DTOs.Outgoing;
 using EliteAPI.Models.Entities.Hypixel;
+using EliteAPI.Models.Entities.Timescale;
 using EliteAPI.Parsers.Farming;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,42 +15,7 @@ public class TimescaleService : ITimescaleService {
     }
 
     public async Task<List<CollectionDataPointDto>> GetCropCollection(Guid memberId, Crop crop, DateTimeOffset start, DateTimeOffset end, int perDay = 4) {
-        // Get all crop collections for the member between the start and end time. 
-        var cropCollection = await _context.CropCollections
-            .Where(cc => cc.ProfileMemberId == memberId && cc.Time >= start && cc.Time <= end)
-            .OrderBy(cc => cc.Time)
-            .ToListAsync();
-        
-        
-        var groupedByDate = cropCollection.GroupBy(obj => obj.Time.Date);
-
-        var selectedEntries = new List<CollectionDataPointDto>();
-
-        foreach (var group in groupedByDate)
-        {
-            // Order the entries in the group by their actual DateTimeOffset values
-            var orderedGroup = group.OrderBy(obj => obj.Time);
-
-            // Calculate the time interval between entries
-            var maxInterval = TimeSpan.FromHours(24) / perDay; // Divide the day into x intervals
-
-            DateTimeOffset? previousTime = null;
-            foreach (var entry in orderedGroup)
-            {
-                if (!previousTime.HasValue || (entry.Time - previousTime.Value) >= maxInterval)
-                {
-                    selectedEntries.Add(new CollectionDataPointDto {
-                        Timestamp = entry.Time.ToUnixTimeSeconds(),
-                        Value = entry.NetherWart
-                    });
-                    previousTime = entry.Time;
-                }
-
-                if (selectedEntries.Count >= 4) break;
-            }
-        }
-
-        return selectedEntries;
+        throw new NotImplementedException();
     }
 
     public async Task<List<CropCollectionsDataPointDto>> GetCropCollections(Guid memberId, DateTimeOffset start, DateTimeOffset end, int perDay = 4) {
@@ -59,36 +25,43 @@ public class TimescaleService : ITimescaleService {
             .OrderBy(cc => cc.Time)
             .ToListAsync();
         
-        
-        var groupedByDate = cropCollection.GroupBy(obj => obj.Time.Date);
+        var groupedByDay = cropCollection.GroupBy(obj => obj.Time.Date);
 
-        var selectedEntries = new List<CropCollectionsDataPointDto>();
+        var selectedEntries = new List<CropCollection>();
 
-        foreach (var group in groupedByDate)
+        foreach (var group in groupedByDay)
         {
-            // Order the entries in the group by their actual DateTimeOffset values
-            var orderedGroup = group.OrderBy(obj => obj.Time);
+            // Order the entries in the group by their actual time values
+            var orderedGroup = group.OrderBy(obj => obj.Time).ToList();
 
-            // Calculate the time interval between entries
-            var maxInterval = TimeSpan.FromHours(24) / perDay; // Divide the day into x intervals
-
-            DateTimeOffset? previousTime = null;
-            foreach (var entry in orderedGroup)
-            {
-                if (!previousTime.HasValue || (entry.Time - previousTime.Value) >= maxInterval)
-                {
-                    selectedEntries.Add(new CropCollectionsDataPointDto {
-                        Timestamp = entry.Time.ToUnixTimeSeconds(),
-                        Crops = entry.ExtractReadableCropCollections()
-                    });
-                    previousTime = entry.Time;
-                }
-
-                if (selectedEntries.Count >= 4) break;
+            // Add all entries if there are less or equal than asked for
+            if (orderedGroup.Count <= perDay) {
+                selectedEntries.AddRange(orderedGroup);
+                continue;
             }
+
+            // Group the entries by the time interval
+            var span = TimeSpan.FromDays(1) / perDay;
+            var groupedByInterval = orderedGroup.GroupBy(obj => obj.Time.Ticks / span.Ticks).ToList();
+            
+            // Add the first entry of each interval
+            selectedEntries.AddRange(groupedByInterval.Select(hourly => hourly.First()));
+
+            // If there are enough groups, we are done here
+            if (groupedByInterval.Count >= perDay) continue;
+ 
+            var intervals = groupedByInterval
+                .Select(g => g.ToList())
+                .Where(g => g.Count > 1).ToList();
+            
+            // Won't guarantee that that the max amount of entries will be returned, and might go over, but it's close enough
+            selectedEntries.AddRange(intervals.Select(g => g.Last()));
         }
 
-        return selectedEntries;
+        return selectedEntries.Select(c => new CropCollectionsDataPointDto {
+            Crops = c.ExtractReadableCropCollections(),
+            Timestamp = c.Time.ToUnixTimeSeconds()
+        }).ToList();
     }
 
     public async Task<List<SkillDataPointDto>> GetSkill(Guid memberId, string skill, DateTimeOffset start, DateTimeOffset end, int perDay = 4) {
