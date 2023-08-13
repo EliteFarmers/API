f70cf151aabead124b9a6ac0de13ea02ab4dc0f8
Kaeso <24925519+ptlthg@users.noreply.github.com>
2023-08-13 19:09:41 -0400

    Yearly Contest Records (#5)
    * Separate player contest queries into new controller
    
    * New dtos
    
    * Make query async

--- EliteAPI/Models/DTOs/Outgoing/Contests.cs
+++ EliteAPI/Models/DTOs/Outgoing/Contests.cs
@@ -6,3 +6,18 @@ public class YearlyContestsDto {
     public bool Complete { get; set; }
     public Dictionary<long, List<string>> Contests { get; set; } = new();
 }
+
+public class YearlyCropRecordsDto {
+    public int Year { get; set; }
+    public Dictionary<string, List<ContestParticipationWithTimestampDto>> Crops { get; set; } = new();
+}
+
+public class ContestParticipationWithTimestampDto {
+    public string PlayerUuid { get; set; } = "";
+    public string PlayerName { get; set; } = "";
+    public string ProfileUuid { get; set; } = "";
+    public long Timestamp { get; set; }
+    public int Collected { get; set; } = 0;
+    public int Position { get; set; } = -1;
+    public int Participants { get; set; } = 0;
+}
