0461c17d2aaf54f6d7d93d7e1205577f0c7b4104
ptlthg <24925519+ptlthg@users.noreply.github.com>
2023-11-09 21:05:06 -0500

    Add background task queue

---
+++ EliteAPI/Services/Background/BackgroundQueueWorker.cs
@@ -0,0 +1,41 @@
+﻿namespace EliteAPI.Services.Background; 
+
+public class BackgroundQueueWorker(
+    IBackgroundTaskQueue queue, 
+    IServiceProvider provider, 
+    ILogger<BackgroundQueueWorker> logger
+) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+        await Task.Yield();
+        
+        logger.LogInformation("Background Task Queue Running");
+        
+        await BackgroundProcessing(stoppingToken);
+    }
+
+    private async Task BackgroundProcessing(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var workItem = await queue.DequeueAsync(stoppingToken);
+
+            try
+            {
+                using var scope = provider.CreateScope();
+                await workItem(scope, stoppingToken);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error occurred executing {WorkItem}", nameof(workItem));
+            }
+        }
+    }
+
+    public override async Task StopAsync(CancellationToken stoppingToken)
+    {
+        logger.LogInformation("Queued Processor is stopping");
+
+        await base.StopAsync(stoppingToken);
+    }
+}
