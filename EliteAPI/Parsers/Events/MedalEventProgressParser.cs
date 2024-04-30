157ddab39b5387b0360b7d5cd89280964bfd0045
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-04-30 10:45:08 -0400

    Assign known brackets for medal events

--- EliteAPI/Parsers/Events/MedalEventProgressParser.cs
+++ EliteAPI/Parsers/Events/MedalEventProgressParser.cs
@@ -12,14 +12,33 @@ public static class MedalEventProgressParser {
 		var start = eventMember.StartTime.ToUnixTimeSeconds();
 		var end = eventMember.EndTime.ToUnixTimeSeconds();
 
-		var medals = from participation in member.JacobData.Contests
+		var participations = from participation in member.JacobData.Contests
 			where participation.JacobContestId >= start && participation.JacobContestId <= end
-			select participation.MedalEarned;
+			select participation;
 		
 		// Count the medals earned by the member in the event
 		var newScore = 0.0;
-		foreach (var medal in medals) {
+		foreach (var participation in participations) {
 			eventMember.Data.ContestParticipations++;
+			var medal = participation.MedalEarned;
+			
+			if (participation.MedalEarned == ContestMedal.None) {
+				// No medal claimed, estimate the medal based the Jacob Contests brackets
+				var contest = participation.JacobContest;
+				
+				if (participation.Collected >= contest.Diamond && contest.Diamond > 0) {
+					medal = ContestMedal.Diamond;
+				} else if (participation.Collected >= contest.Gold && contest.Gold > 0) {
+					medal = ContestMedal.Gold;
+				} else if (participation.Collected >= contest.Silver && contest.Silver > 0) {
+					medal = ContestMedal.Silver;
+				} else if (participation.Collected >= contest.Bronze && contest.Bronze > 0) {
+					medal = ContestMedal.Bronze;
+				} else {
+					medal = ContestMedal.None;
+				}
+			}
+			
 			if (!weights.ContainsKey(medal)) continue;
 			
 			newScore += weights[medal];
