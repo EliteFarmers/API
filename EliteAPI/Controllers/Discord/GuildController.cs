f17b17d4ddc8557846a2cf4ed69c806e25be483a
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-05-21 21:28:46 -0400

    Update discord controllers

--- EliteAPI/Controllers/Discord/GuildController.cs
+++ EliteAPI/Controllers/Discord/GuildController.cs
@@ -10,31 +10,29 @@ namespace EliteAPI.Controllers.Discord;
 
 [Route("[controller]")]
 [ApiController]
-public class GuildController : Controller {
-    private readonly IDiscordService _discordService;
-    private readonly DataContext _context;
-    private readonly IMapper _mapper;
-
-    public GuildController(DataContext context, IMapper mapper, IDiscordService discordService) {
-        _context = context;
-        _mapper = mapper;
-        _discordService = discordService;
-    }
-    
-    // GET <GuildController>s
+public class GuildController(
+    DataContext context, 
+    IMapper mapper, 
+    IDiscordService discordService)
+    : Controller 
+{
+    /// <summary>
+    /// Get list of public guilds
+    /// </summary>
+    /// <returns></returns>
     [Route("/[controller]s")]
     [HttpGet]
     [ResponseCache(Duration = 60 * 60 * 24, Location = ResponseCacheLocation.Any)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<List<GuildDetailsDto>>> GetGuilds() {
-        var guilds = await _context.Guilds
+        var guilds = await context.Guilds
             .Where(g => g.InviteCode != null && g.Features.JacobLeaderboardEnabled)
             .OrderByDescending(g => g.MemberCount)
             .Select(g => new GuildDetailsDto {
                 Id = g.Id.ToString(), 
-                Name = g.Name, 
-                InviteCode = g.InviteCode, 
-                Banner = g.Banner, 
+                Name = g.Name,
+                InviteCode = g.InviteCode,
+                Banner = g.Banner,
                 Icon = g.Icon,
                 MemberCount = g.MemberCount
             })
@@ -43,7 +41,11 @@ public class GuildController : Controller {
         return Ok(guilds);
     }
     
-    // GET <GuildController>/[guildId]
+    /// <summary>
+    /// Get guild by ID
+    /// </summary>
+    /// <param name="guildId">Discord server (guild) ID</param>
+    /// <returns></returns>
     [HttpGet("{guildId}")]
     [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
     [ProducesResponseType(StatusCodes.Status200OK)]
@@ -52,15 +54,19 @@ public class GuildController : Controller {
     public async Task<ActionResult<PublicGuildDto>> GetGuildById(long guildId) {
         if (guildId <= 0) return BadRequest("Invalid guild ID.");
 
-        await _discordService.RefreshBotGuilds();
+        await discordService.RefreshBotGuilds();
         
-        var guild = await _context.Guilds.FindAsync((ulong) guildId);
+        var guild = await context.Guilds.FindAsync((ulong) guildId);
         if (guild is null) return NotFound("Guild not found.");
         
-        return Ok(_mapper.Map<PublicGuildDto>(guild));
+        return Ok(mapper.Map<PublicGuildDto>(guild));
     }
     
-    // GET <GuildController>/[guildId]/Events
+    /// <summary>
+    /// Get guild events
+    /// </summary>
+    /// <param name="guildId">Discord server (guild) ID</param>
+    /// <returns></returns>
     [HttpGet("{guildId}/Events")]
     [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
     [ProducesResponseType(StatusCodes.Status200OK)]
@@ -69,17 +75,17 @@ public class GuildController : Controller {
     public async Task<ActionResult<List<EventDetailsDto>>> GetGuildEvents(long guildId) {
         if (guildId <= 0) return BadRequest("Invalid guild ID.");
 
-        await _discordService.RefreshBotGuilds();
+        await discordService.RefreshBotGuilds();
         
-        var guild = await _context.Guilds.FindAsync((ulong) guildId);
+        var guild = await context.Guilds.FindAsync((ulong) guildId);
         if (guild is null) return NotFound("Guild not found.");
         
-        var events = await _context.Events
+        var events = await context.Events
             .Where(e => e.GuildId == guild.Id)
             .OrderBy(e => e.StartTime)
             .AsNoTracking()
             .ToListAsync();
         
-        return Ok(_mapper.Map<List<EventDetailsDto>>(events) ?? new List<EventDetailsDto>());
+        return Ok(mapper.Map<List<EventDetailsDto>>(events) ?? []);
     }
 }
