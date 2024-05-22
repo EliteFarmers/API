8f09b5b55a1ec20c172878fc5ad91a5eb910c3fd
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-05-21 20:00:41 -0400

    Update admin and badge controllers

--- EliteAPI/Controllers/BadgeController.cs
+++ EliteAPI/Controllers/BadgeController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using EliteAPI.Data;
 using EliteAPI.Models.DTOs.Incoming;
+using EliteAPI.Models.Entities.Accounts;
+using EliteAPI.Services.BadgeService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,8 +11,17 @@ namespace EliteAPI.Controllers;
 
 [Route("[controller]")]
 [ApiController]
-public class BadgeController(DataContext context, IMapper mapper) : ControllerBase {
+public class BadgeController(
+    DataContext context,
+    IBadgeService badgeService,
+    IMapper mapper)
+: ControllerBase
+{
     
+    /// <summary>
+    /// Get all badges
+    /// </summary>
+    /// <returns></returns>
     [Route("/[controller]s")]
     [HttpGet]
     [ResponseCache(Duration = 60 * 60 * 24, Location = ResponseCacheLocation.Any)]
@@ -22,4 +34,126 @@ public class BadgeController(DataContext context, IMapper mapper) : ControllerBa
         return Ok(mapper.Map<List<BadgeDto>>(badges));
     }
     
+    /// <summary>
+    /// Add a badge to a user
+    /// </summary>
+    /// <param name="playerUuid">Player UUID (no hyphens)</param>
+    /// <param name="badgeId">Badge ID</param>
+    /// <returns></returns>
+    [Authorize("Moderator")]
+    [HttpPost("user/{playerUuid}/{badgeId:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public async Task<ActionResult> AddPlayerBadge(string playerUuid, int badgeId) {
+        return await badgeService.AddBadgeToUser(playerUuid, badgeId);
+    }
+    
+    /// <summary>
+    /// Remove a badge from a user
+    /// </summary>
+    /// <param name="playerUuid">Player UUID (no hyphens)</param>
+    /// <param name="badgeId">Badge ID</param>
+    /// <returns></returns>
+    [Authorize("Moderator")]
+    [HttpDelete("user/{playerUuid}/{badgeId:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public async Task<ActionResult> RemovePlayerBadge(string playerUuid, int badgeId) {
+        return await badgeService.RemoveBadgeFromUser(playerUuid, badgeId);
+    }
+    
+    /// <summary>
+    /// Create a new badge
+    /// </summary>
+    /// <param name="badge">Badge information</param>
+    /// <returns></returns>
+    [Authorize("Admin")]
+    [HttpPost("[controller]/Badges")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public async Task<ActionResult> CreateBadge([FromBody] CreateBadgeDto badge) {
+        var newBadge = new Badge {
+            Name = badge.Name,
+            Description = badge.Description,
+            ImageId = badge.ImageId,
+            Requirements = badge.Requirements,
+            TieToAccount = badge.TieToAccount
+        };
+        
+        context.Badges.Add(newBadge);
+        
+        await context.SaveChangesAsync();
+        
+        return Ok();
+    }
+    
+    /// <summary>
+    /// Edit a badge
+    /// </summary>
+    /// <param name="badgeId">Badge ID</param>
+    /// <param name="badge">New values</param>
+    /// <returns></returns>
+    [Authorize("Admin")]
+    [HttpPatch("{badgeId:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public async Task<ActionResult> EditBadge(int badgeId, [FromBody] EditBadgeDto badge) {
+        if (HttpContext.Items["Account"] is not EliteAccount account || HttpContext.Items["DiscordToken"] is not string) {
+            return Unauthorized("Account not found.");
+        }
+        
+        if (!account.Permissions.HasFlag(PermissionFlags.Admin)) {
+            return Unauthorized("You do not have permission to do this!");
+        }
+        
+        var existingBadge = await context.Badges
+            .FirstOrDefaultAsync(b => b.Id == badgeId);
+        
+        if (existingBadge is null) {
+            return NotFound("Badge not found.");
+        }
+        
+        existingBadge.Name = badge.Name ?? existingBadge.Name;
+        existingBadge.Description = badge.Description ?? existingBadge.Description;
+        existingBadge.ImageId = badge.ImageId ?? existingBadge.ImageId;
+        existingBadge.Requirements = badge.Requirements ?? existingBadge.Requirements;
+        
+        await context.SaveChangesAsync();
+        
+        return Ok();
+    }
+    
+    /// <summary>
+    /// Delete a badge
+    /// </summary>
+    /// <param name="badgeId">Badge ID</param>
+    /// <returns></returns>
+    [Authorize("Admin")]
+    [HttpDelete("{badgeId:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(string))]
+    public async Task<ActionResult> DeleteBadge(int badgeId) {
+        var existingBadge = await context.Badges
+            .FirstOrDefaultAsync(b => b.Id == badgeId);
+        
+        if (existingBadge is null) {
+            return NotFound("Badge not found.");
+        }
+        
+        context.Badges.Remove(existingBadge);
+        
+        await context.SaveChangesAsync();
+        
+        return Ok();
+    }
 }
--- EliteAPI/Models/DTOs/Auth/DiscordLoginDto.cs
+++ EliteAPI/Models/DTOs/Auth/DiscordLoginDto.cs
@@ -14,8 +14,11 @@ public class DiscordLoginDto {
 public class AuthResponseDto {
 	[JsonPropertyName("access_token")]
 	public required string AccessToken { get; set; }
+	
 	[JsonPropertyName("expires_in")]
-	public required string ExpiresIn { get; set; }
+	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+	public string? ExpiresIn { get; set; }
+	
 	[JsonPropertyName("refresh_token")]
 	public required string RefreshToken { get; set; }
 }
