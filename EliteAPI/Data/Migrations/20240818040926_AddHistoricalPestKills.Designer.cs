090e110ecf9c1076e6f5569b9ce0ac4fbca020a4
ptlthg <24925519+ptlthg@users.noreply.github.com>
2024-08-18 02:17:26 -0400

    Add migrations and configure timescale compression

--- EliteAPI/Data/Migrations/20240818040926_AddHistoricalPestKills.Designer.cs
+++ EliteAPI/Data/Migrations/20240818040926_AddHistoricalPestKills.Designer.cs
@@ -12,6 +12,7 @@ using EliteAPI.Models.Entities.Monetization;
 using HypixelAPI.DTOs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -20,9 +21,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EliteAPI.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240818040926_AddHistoricalPestKills")]
+    partial class AddHistoricalPestKills
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EliteAPI/Data/Migrations/20240818040926_AddHistoricalPestKills.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EliteAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoricalPestKills : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Beetle",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Cricket",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Earthworm",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Fly",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Locust",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mite",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mosquito",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Moth",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rat",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Slug",
+                table: "CropCollections",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+             migrationBuilder.Sql(
+                 $"""
+                     SELECT create_hypertable('"CropCollections"', 'Time', migrate_data => true);
+                 
+                     SELECT set_chunk_time_interval('"CropCollections"', INTERVAL '24 hours');
+                     ALTER TABLE "CropCollections" SET (
+                         timescaledb.compress = true,
+                         timescaledb.compress_segmentby = '"ProfileMemberId"',
+                         timescaledb.compress_chunk_time_interval = '24 hours'
+                     );
+                     
+                     SELECT add_compression_policy('"CropCollections"', INTERVAL '1 month');
+                 """);
+            
+            migrationBuilder.Sql(
+                $"""
+                     SELECT create_hypertable('"SkillExperiences"', 'Time', migrate_data => true);
+                 
+                     SELECT set_chunk_time_interval('"SkillExperiences"', INTERVAL '24 hours');
+                     ALTER TABLE "SkillExperiences" SET (
+                 	    timescaledb.compress = true,
+                 	    timescaledb.compress_segmentby = '"ProfileMemberId"',
+                 	    timescaledb.compress_chunk_time_interval = '24 hours'
+                     );
+                     
+                     SELECT add_compression_policy('"SkillExperiences"', INTERVAL '1 month');
+                 """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Beetle",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Cricket",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Earthworm",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Fly",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Locust",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Mite",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Mosquito",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Moth",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Rat",
+                table: "CropCollections");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "CropCollections");
+        }
+    }
+}
