f1484cec372086195c9f3916a25ca312d0496bf5
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-04-02 00:59:58 -0400

    Adjust mapping

--- EliteAPI/Features/Leaderboards/LeaderboardMapper.cs
+++ EliteAPI/Features/Leaderboards/LeaderboardMapper.cs
@@ -10,6 +10,7 @@ public static partial class LeaderboardMapper {
 	[MapperIgnoreSource(nameof(LeaderboardEntry.MemberId))]
 	[MapperIgnoreTarget(nameof(LeaderboardEntryDto.MembersSerializationHelper))]
 	[MapperIgnoreTarget(nameof(LeaderboardEntryDto.Removed))]
+	[MapperIgnoreTarget(nameof(LeaderboardEntryDto.InitialAmount))]
 	public static partial LeaderboardEntryDto MapToDto(this LeaderboardEntry entry);
 	
 	public static partial LeaderboardEntryWithRankDto MapToDto(this LeaderboardEntryWithRankDto entry);
@@ -20,6 +21,9 @@ public static partial class LeaderboardMapper {
 	[MapperIgnoreTarget(nameof(LeaderboardDto.MaxEntries))]
 	[MapperIgnoreTarget(nameof(LeaderboardDto.Entries))]
 	[MapperIgnoreTarget(nameof(LeaderboardDto.ShortTitle))]
+	[MapperIgnoreTarget(nameof(LeaderboardDto.MinimumScore))]
+	[MapperIgnoreTarget(nameof(LeaderboardDto.StartsAt))]
+	[MapperIgnoreTarget(nameof(LeaderboardDto.EndsAt))]
 	public static partial LeaderboardDto MapToDto(this Leaderboard leaderboard);
 	
 	public static partial ProfileLeaderboardMemberDto MapToDto(this ProfileLeaderboardMember dto);
