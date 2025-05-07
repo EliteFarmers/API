aa125f2d2423cb055e423cdcaf71396249364e0f
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-05-07 14:31:00 -0400

    Add monthly farming xp leaderboard

--- EliteAPI/Features/Leaderboards/Definitions/Skills/SkillFarmingLeaderboard.cs
+++ EliteAPI/Features/Leaderboards/Definitions/Skills/SkillFarmingLeaderboard.cs
@@ -10,11 +10,12 @@ public class SkillFarmingLeaderboard : IMemberLeaderboardDefinition {
 		Slug = "farming",
 		Category = "Skills",
 		MinimumScore = 1_000_000,
-		IntervalType = [LeaderboardType.Current],
+		IntervalType = [LeaderboardType.Current, LeaderboardType.Monthly],
 		ScoreDataType = LeaderboardScoreDataType.Decimal
 	};
 
 	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Skills) return 0;
 		return (decimal) member.Skills.Farming;
 	}
 }
