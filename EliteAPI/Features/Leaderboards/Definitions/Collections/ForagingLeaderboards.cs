cee4bf72e87c5c57bef0566017ea55f816d7b98e
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-06-23 11:31:31 -0400

    Foraging update leaderboards

--- EliteAPI/Features/Leaderboards/Definitions/Collections/ForagingLeaderboards.cs
+++ EliteAPI/Features/Leaderboards/Definitions/Collections/ForagingLeaderboards.cs
@@ -129,3 +129,129 @@ public class SpruceLeaderboard : IMemberLeaderboardDefinition {
 		return collection;
 	}
 }
+
+public class SeaLumiesLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Sea Lumies Collection",
+		ShortTitle = "Sea Lumies",
+		Slug = "sea-lumies",
+		Category = "Foraging",
+		MinimumScore = 5_000,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "SEA_LUMIES";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
+
+public class VinesapLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Vinesap Collection",
+		ShortTitle = "Vinesap",
+		Slug = "vinesap",
+		Category = "Foraging",
+		MinimumScore = 1_000,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "VINESAP";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
+
+public class LushlilacLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Lushlilac Collection",
+		ShortTitle = "Lushlilac",
+		Slug = "lushlilac",
+		Category = "Foraging",
+		MinimumScore = 500,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "LUSHLILAC";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
+
+public class MangroveLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Mangrove Wood Collection",
+		ShortTitle = "Mangrove",
+		Slug = "mangrove",
+		Category = "Foraging",
+		MinimumScore = 100_000,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "MANGROVE_LOG";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
+
+public class FigLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Fig Wood Collection",
+		ShortTitle = "Fig",
+		Slug = "fig",
+		Category = "Foraging",
+		MinimumScore = 100_000,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "FIG_LOG";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
+
+public class TenderWoodLeaderboard : IMemberLeaderboardDefinition {
+	public LeaderboardInfo Info { get; } = new() {
+		Title = "Tender Wood Collection",
+		ShortTitle = "Tender Wood",
+		Slug = "tender-wood",
+		Category = "Foraging",
+		MinimumScore = 1_000,
+		IntervalType = [LeaderboardType.Current],
+		ScoreDataType = LeaderboardScoreDataType.Long,
+	};
+
+	private const string CollectionId = "TENDER_WOOD";
+	
+	public decimal GetScoreFromMember(ProfileMember member, LeaderboardType type) {
+		if (type != LeaderboardType.Current && !member.Api.Collections) return 0;
+
+		member.TryGetCollection(CollectionId, out var collection);
+		return collection;
+	}
+}
