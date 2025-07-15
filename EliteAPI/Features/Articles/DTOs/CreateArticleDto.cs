577af9d0d07de7d132e6549ebe6a659fc96e41ce
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-07-15 19:23:19 -0400

    Remove articles from this branch (out of scope)

--- EliteAPI/Features/Articles/DTOs/CreateArticleDto.cs
+++
@@ -1,7 +0,0 @@
-namespace EliteAPI.Features.Articles;
-
-public class CreateArticleDto
-{
-    public required string Title { get; set; } = string.Empty;
-    public required string Content { get; set; } = string.Empty;
-}
