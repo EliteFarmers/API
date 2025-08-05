1db63f840a758411ce5be5a67fd755342079fb28
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-08-04 22:57:32 -0400

    Start setting up integration tests

---
+++ EliteAPI.Tests/App.cs
@@ -0,0 +1,9 @@
+global using FastEndpoints.Testing;
+global using Shouldly;
+global using System.Net;
+global using Xunit;
+global using Microsoft.Extensions.DependencyInjection;
+
+namespace EliteAPI.Tests;
+
+public class App : AppFixture<Program>;
--- EliteAPI/Program.cs
+++ EliteAPI/Program.cs
@@ -203,3 +203,5 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
+
+public partial class Program;
