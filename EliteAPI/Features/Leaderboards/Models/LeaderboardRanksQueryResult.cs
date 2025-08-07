f391885c5f230e34717d222e9039df861ba914af
ptlthg <24925519+ptlthg@users.noreply.github.com>
2025-08-06 22:10:23 -0400

    Use a window function + CTE for calculating ranks

---
+++ EliteAPI/Features/Leaderboards/Models/LeaderboardRanksQueryResult.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EliteAPI.Features.Leaderboards.Models;
+
+public class LeaderboardRanksQueryResult {
+	public string? IntervalIdentifier { get; set; }
+	public decimal Score { get; set; }
+	public decimal InitialScore { get; set; }
+	public long Rank { get; set; }
+	public required string Title { get; set; }
+	public required string Slug { get; set; }
+	public required string ShortTitle { get; set; }
+	public required string ScoreDataType { get; set; }
+}
+
+public class LeaderboardRanksQueryResultConfiguration : IEntityTypeConfiguration<LeaderboardRanksQueryResult>
+{
+	public void Configure(EntityTypeBuilder<LeaderboardRanksQueryResult> builder)
+	{
+		builder.HasNoKey();
+		builder.ToTable("LeaderboardRanksQueryResult", t => t.ExcludeFromMigrations());
+	}
+}
