30632e3f47f4908de0acafcc43c57560bc5f6a4d
ptlthg <24925519+ptlthg@users.noreply.github.com>
2026-01-28 01:13:42 -0500

    Check if we can reenable the endpoint

--- EliteAPI/Features/Leaderboards/Endpoints/GetPlayerRank/Endpoint.cs
+++ EliteAPI/Features/Leaderboards/Endpoints/GetPlayerRank/Endpoint.cs
@@ -23,17 +23,17 @@ internal sealed class GetPlayerRankEndpoint(
 		
 		// SkyHanni version 6.13.0 requests this endpoint way too often causing performance issues
 		// Temporary fix to disable responses for this version
-		if (HttpContext.GetSkyHanniVersion() is { } version && version == new Version("6.13.0")) {
-			await Send.OkAsync(new LeaderboardPositionDto {
-				Rank = -1,
-				Amount = 0,
-				MinAmount = lbService.GetLeaderboardMinScore(request.Leaderboard),
-				UpcomingRank = 10_000,
-				UpcomingPlayers = []
-			}, c);
+		// if (HttpContext.GetSkyHanniVersion() is { } version && version == new Version("6.13.0")) {
+		// 	await Send.OkAsync(new LeaderboardPositionDto {
+		// 		Rank = -1,
+		// 		Amount = 0,
+		// 		MinAmount = lbService.GetLeaderboardMinScore(request.Leaderboard),
+		// 		UpcomingRank = 10_000,
+		// 		UpcomingPlayers = []
+		// 	}, c);
 
-			return;
-		}
+		// 	return;
+		// }
 		
 #pragma warning disable CS0618 // Type or member is obsolete
 		if (request is { IncludeUpcoming: true, Upcoming: 0 or null }) request.Upcoming = 10;
